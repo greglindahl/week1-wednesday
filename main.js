fc3f64af7f4965fe72d28cfc3f71b952d8f33f22
greglindahl <greglindahl410@gmail.com>
2015-09-17 13:18:40 -0600

    Completed tasks 1 - 12

--- main.js
+++ main.js
@@ -8,38 +8,66 @@
 // If you would like, practice using document.write() as well. Use any elements you like!
 // ==========================================================================
 
-// 1. Declare a variable whose value is an empty Array. Use any method you choose to add at least 4 items to it.
+console.log("Question 1. Declare a variable whose value is an empty Array. Use any method you choose to add at least 4 items to it.");
 
-//console.log("Question 1");
-//console.log(yourAnswer);
+var myArray = [];
+myArray.push(1, 2, 3, 4);
 
+console.log(myArray);
 
-// 2. Add an additional item to the beginning of your Array.
+console.log("Question 2. Add an additional item to the beginning of your Array.");
+myArray.unshift(5);
+console.log(myArray);
 
-// 3. Remove the second and third items.
+console.log("Question 3. Remove the second and third items.");
+myArray.splice(2, 3);
+console.log(myArray);
 
-// 4. Add two new items after the second item.
+console.log("Question 4. Add two new items after the second item.");
+myArray.splice(2, 0, 6, 7);
+console.log(myArray);
 
-// 5. Write 'The current length of the array is....' using the .length method
+console.log("Question 5. Write 'The current length of the array is....' using the .length method");
+console.log('The current length of the array is ' + myArray.length);
 
 // Use the following Array for questions 6-11:
 var things = ['mug', 'book', 'mouse', 'plant', 'sunglasses'];
 
-// 6. Change 'mouse' to 'keyboard'
+console.log("Question 6. Change 'mouse' to 'keyboard'");
+things[2] = 'keyboard';
+console.log(things) 
 
-// 7. Combine all of the elements of the array into a string.
+console.log("Question 7. Combine all of the elements of the array into a string.");
+var thingsToString = things.toString();
+console.log(thingsToString);
 
-// 8. Declare a variable called lastItem whose using .pop() Add two new items to lastItem, on at the beginning and one at the end.
+console.log("Question 8. Declare a variable called lastItem whose using .pop() Add two new items to lastItem, on at the beginning and one at the end.");
+var things = ['mug', 'book', 'mouse', 'plant', 'sunglasses'];
+var lastItem = things.pop();
+var myLastArray = [];
+myLastArray.push(lastItem);
+myLastArray.unshift('pencil');
+myLastArray.push('dog');
+console.log(myLastArray);
 
-// 9. Create a new Array called itemLast. The items should be the same as lastItem, only in reverse order.
+console.log("Question 9. Create a new Array called itemLast. The items should be the same as lastItem, only in reverse order.");
+var itemLast = myLastArray.reverse();
+console.log(itemLast);
 
-// 10. Remove the first item of itemLast.
+console.log('Question 10. Remove the first item of itemLast.');
+itemLast.shift();
+console.log(itemLast);
 
-// 11. Remove all items from itemLast (No need to write to the document. Just console.log to test your results)
+console.log('Question 11. Remove all items from itemLast');
+itemLast = [];
+console.log(itemLast);
 
 // 12. Using the Arrays below, create a single Array called numberPets whose value is [12, 5, 9, 27, 'fish', 'dog']
 var firstArray = [12, 5, 9, 27];
 var secondArray = ['fish', 'dog'];
+var numberPets = [];
+numberPets.push(firstArray + secondArray);
+console.log(numberPets);
 
 // Use the following array for questions 13-16:
 var people = ['Bill', 'Ted', 'Emily', 'Andrea', 'Doug'];
